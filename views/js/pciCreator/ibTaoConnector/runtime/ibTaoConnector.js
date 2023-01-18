2ac3ae05ba8789864e65c5b2c0a1e8303df79d6e
fwagner <fwagner@dipf.de>
2023-01-18 16:40:42 +0100

    empty response / navlock

--- views/js/pciCreator/ibTaoConnector/runtime/ibTaoConnector.js
+++ views/js/pciCreator/ibTaoConnector/runtime/ibTaoConnector.js
@@ -260,6 +260,8 @@ define(['qtiCustomInteractionContext',
                 _response['logs'] = this.traceLogs;
             }
 
+            if(!_response['score'] && !_response['logs'])
+                return { base: null };
 
             return  {
                 base : {
